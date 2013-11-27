c46f9abaa20a29740f1e8500663a848bcf1798a0
Cassio Cabral <cassioscabral@gmail.com>
2013-11-27 17:00:33 -0300

    end sobrando em engine.rb

--- lib/spree2_pagseguro/engine.rb
+++ lib/spree2_pagseguro/engine.rb
@@ -1,25 +1,24 @@
 module Spree2Pagseguro
-    require 'spree/core'
-    isolate_namespace Spree
-    engine_name 'spree2_pagseguro'
+  require 'spree/core'
+  isolate_namespace Spree
+  engine_name 'spree2_pagseguro'
 
-    config.autoload_paths += %W(#{config.root}/lib)
+  config.autoload_paths += %W(#{config.root}/lib)
 
-    # use rspec for tests
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-
-    initializer "spree.register.pagseguro_payment_method", after: "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::PaymentMethod::Pagseguro
-    end
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare &method(:activate).to_proc
+  # use rspec for tests
+  config.generators do |g|
+    g.test_framework :rspec
   end
+
+  initializer "spree.register.pagseguro_payment_method", after: "spree.register.payment_methods" do |app|
+    app.config.spree.payment_methods << Spree::PaymentMethod::Pagseguro
+  end
+
+  def self.activate
+    Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+      Rails.configuration.cache_classes ? require(c) : load(c)
+    end
+  end
+
+  config.to_prepare &method(:activate).to_proc
 end
