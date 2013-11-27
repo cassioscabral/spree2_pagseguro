e877f7ed8a488b02834afbc0ee4830c40d030d18
Cassio Cabral <cassioscabral@gmail.com>
2013-11-27 17:13:39 -0300

    adicionado a classe Engine

--- lib/spree2_pagseguro/engine.rb
+++ lib/spree2_pagseguro/engine.rb
@@ -1,23 +1,27 @@
-module Spree2Pagseguro
-  require 'spree/core'
-  engine_name 'spree2_pagseguro'
+module Spree
+  module Spree2Pagseguro
+    class Engine < Rails::Engine
+      require 'spree/core'
+      engine_name 'spree2_pagseguro'
 
-  config.autoload_paths += %W(#{config.root}/lib)
+      config.autoload_paths += %W(#{config.root}/lib)
 
-  # use rspec for tests
-  config.generators do |g|
-    g.test_framework :rspec
-  end
+      # use rspec for tests
+      config.generators do |g|
+        g.test_framework :rspec
+      end
 
-  initializer "spree.register.pagseguro_payment_method", after: "spree.register.payment_methods" do |app|
-    app.config.spree.payment_methods << Spree::PaymentMethod::Pagseguro
-  end
+      initializer "spree.register.pagseguro_payment_method", after: "spree.register.payment_methods" do |app|
+        app.config.spree.payment_methods << Spree::PaymentMethod::Pagseguro
+      end
 
-  def self.activate
-    Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-      Rails.configuration.cache_classes ? require(c) : load(c)
+      def self.activate
+        Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+          Rails.configuration.cache_classes ? require(c) : load(c)
+        end
+      end
+
+      config.to_prepare &method(:activate).to_proc
     end
   end
-
-  config.to_prepare &method(:activate).to_proc
 end
