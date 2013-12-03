b5e43e1657c0c90422febb593ee04ae3d99b70c5
Cassio Cabral <cassioscabral@gmail.com>
2013-12-02 23:29:30 -0300

    fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.prepend do
-  match "pag_seguro/notify", :to => "pag_seguro#notify", method: :post
-  match "pag_seguro/callback", :to => "pag_seguro#callback", method: :get
+  match "pag_seguro/notify", :to => "pag_seguro#notify", via: [:post]
+  match "pag_seguro/callback", :to => "pag_seguro#callback", via: [:get]
   resources :payment_notifications, :only => [:create]
 end
