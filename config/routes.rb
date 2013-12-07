1c1969b07a052d93e8f175edb4dcff061c48b28a
Cassio Cabral <cassioscabral@gmail.com>
2013-12-07 17:35:56 -0300

    alterado as rotas para  serem adicionadas a engine do spree

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.add_routes do
   match "pag_seguro/notify", :to => "pag_seguro#notify", via: :post
   match "pag_seguro/callback", :to => "pag_seguro#callback", via: :get
   resources :payment_notifications, :only => [:create]
